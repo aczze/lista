05cc5f835357986c77a4a1fcc36913f3bf63c9ce
aczze <38169398+aczze@users.noreply.github.com>
2018-11-02 12:19:51 +0100

    Add files via upload

---
+++ lista.h
@@ -0,0 +1,28 @@
+/*
+ * lista.h
+ *
+ *  Created on: Oct 31, 2018
+ *      Author: marcin
+ */
+
+#ifndef LISTA_H_
+#define LISTA_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct nodeS {
+	int data;
+	struct nodeS* next; //ZMIENNA NEXT JEST WSKAZNIKIEM DO NODE'A. Nie kumam do konca tego zapisu
+} nodeS;
+
+typedef struct list {
+	nodeS* head;
+}list;
+
+int push(list* l, int data1);
+void print_all(list* l);
+void pop_front(list* l);
+void pop_back(list* l);
+
+#endif /* LISTA_H_ */
---
+++ mian.c
@@ -0,0 +1,19 @@
+#include "lista.h"
+#include "criterion.h"
+
+//int main(void)
+//{
+//	list* l = malloc(sizeof(*l)); //Rezerwuje komorke pamieci dla struktury o zmiennej 'list' ktora jest wielkosci '*l'
+//	l->head = NULL;
+//	push(l, 1);
+//	push(l, 2);
+//	push(l, 3);
+//
+//	printf("\nAby usunac pierwszy element kliknij: '1'\nAby usunac ostatni element kliknij: '2'\n\n");
+//
+//	pop_front(l);
+//	print_all(l);l
+//	return 0;
+//}
+
+
